b7ac3d4adeb5c329d97593c83c150f00404bba20
Matt Javier <matthewrjavier@gmail.com>
2020-11-02 21:14:37 -0800

    updates to server.js file

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ require('dontenv').config()
 const express = require('express')
 const { join } = require('path')
 const passport = require('passport')
+const { Strategy } = require('passport-local')
+const { Strategy: JWTStrategy, ExtractJWT } = require('passport-jwt')
+const { User } = require('./models')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
